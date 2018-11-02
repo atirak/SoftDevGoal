ff15302fd2ba87840ab7afc0575864495971a7ed
atirak <59160624@go.buu.ac.th>
2018-11-02 13:54:24 +0700

    update

--- src/myapp/Main.java
+++ src/myapp/Main.java
@@ -1,6 +1,15 @@
 
 package myapp;
 
+import javax.swing.JFrame;
+
 public class Main {
-    
+     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        
+        frame.pack();
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);        
+        frame.setVisible(true);
+        
+    }
 }
---
+++ src/myapp/Orders.java
@@ -0,0 +1,12 @@
+
+package myapp;
+
+ 
+public class Orders {
+    String bookname;
+    String bookprice;
+    Orders(String bookname,String bookprice){
+        this.bookname=bookname;
+        this.bookprice=bookprice;
+    }
+}
